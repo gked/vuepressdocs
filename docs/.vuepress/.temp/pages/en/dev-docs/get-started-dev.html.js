36b42b9ccbc3d113da5e33ed0c144ae7d7d80036
gked <glyukshin@mailfence.com>
2024-09-27 19:20:20 -0700

    l

--- docs/.vuepress/.temp/pages/en/dev-docs/get-started-dev.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "/home/user/projects/vuepressdocs/docs/.vuepress/.temp/pages/en/dev-docs/get-started-dev.html.vue"
-const data = JSON.parse("{\"path\":\"/en/dev-docs/get-started-dev.html\",\"title\":\"Developer Documentation\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1727415810000,\"contributors\":[{\"name\":\"user\",\"email\":\"user@pop-os.localdomain\",\"commits\":1}]},\"filePathRelative\":\"en/dev-docs/get-started-dev.md\"}")
-export { comp, data }
--- docs/.vuepress/.temp/pages/en/user-docs/get-started-user.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "/home/user/projects/vuepressdocs/docs/.vuepress/.temp/pages/en/user-docs/get-started-user.html.vue"
-const data = JSON.parse("{\"path\":\"/en/user-docs/get-started-user.html\",\"title\":\"User documentation\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1727415810000,\"contributors\":[{\"name\":\"user\",\"email\":\"user@pop-os.localdomain\",\"commits\":1}]},\"filePathRelative\":\"en/user-docs/get-started-user.md\"}")
-export { comp, data }
